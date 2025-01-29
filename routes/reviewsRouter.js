b489b0e32b5845b296db5260879bb34d56c42c96
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2025-01-29 11:18:19 +0200

    delete

--- routes/reviewsRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require ("express");
-const reviewsRouter = express.Router();
-const ctrl = require('../controllers/rewiesControllers');
-
-reviewsRouter.get("/", ctrl.getReviews);
-
-module.exports = reviewsRouter;
--- routes/storesRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require ("express");
-const storenearestRouter = express.Router();
-const ctrl = require('../controllers/storesControllers');
-
-
-storenearestRouter.get("/nearest", ctrl.getStoresNearest);
-storenearestRouter.get("/", ctrl.getStores);
-
-module.exports = storenearestRouter;
